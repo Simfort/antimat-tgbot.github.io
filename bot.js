83baa9d519136647380d115db69ad6de7d6c5945
Artoym Lemonjava <lakdavbaby@gmail.com>
2024-10-15 21:01:32 +0300

    other commit

--- bot.js
+++ bot.js
@@ -2,17 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const grammy_1 = require("grammy");
 const bot = new grammy_1.Bot('7795476268:AAHp-I49ugHelQKOkBBTrlQITsQqevBNjo4');
+const link = `https://www.youtube.com/watch?v=FCHNnZ2KDUQ`;
 bot.api.setMyCommands([
-    { command: 'start', description: 'Start the bot' },
+    { command: 'start', description: 'Запусти бота' },
+    { command: 'main', description: 'Панель' }
 ]);
 bot.command('start', async (ctx) => {
     await ctx.reply('Приветсвую меня зовут @NewBotSim');
 });
-bot.hears([/бля/, /сука/, /ебан/, /гондон/, /хуй/, /пизда/], async (ctx) => {
-    await bot.api.deleteMessage(ctx.chat.id, ctx.msgId);
-    await ctx.reply(`@${ctx.from.username} не матерись!`);
+bot.command('main', async (ctx) => {
+    const mainkeyboard = new grammy_1.InlineKeyboard().webApp('App', link);
+    await ctx.reply('Передаю вам свой пульт!', {
+        reply_markup: mainkeyboard
+    });
 });
-bot.hears([/тёма/, /тема/, /артем/], async (ctx) => {
-    await ctx.reply('Тема крутой !')
-})
+bot.hears([/блять/, /сука/, /ебан/, /гондон/, /хуй/, /пизда/], async (ctx) => {
+    await bot.api.deleteMessage(ctx.chat.id, ctx.msgId);
+    await ctx.reply('Не матерись!');
+});
+bot.hears('О нас', async (ctx) => {
+    await ctx.reply('Автор бота:@simfort');
+});
+bot.hears('Сыллка на группу автора', async (ctx) => {
+    await ctx.reply('https://t.me/getcode_front');
+});
 bot.start();
--- bot.ts
+++ bot.ts
@@ -1,24 +1,31 @@
-import { Bot, Keyboard } from 'grammy'
+import { Bot, InlineKeyboard, Keyboard } from 'grammy'
 
 const bot = new Bot('7795476268:AAHp-I49ugHelQKOkBBTrlQITsQqevBNjo4')
-
+const link = `https://www.youtube.com/watch?v=FCHNnZ2KDUQ`
 bot.api.setMyCommands([
     { command: 'start', description: 'Запусти бота' },
-    { command: 'all', description: 'Панель' }
+    { command: 'main', description: 'Панель' }
 ])
 
 bot.command('start', async (ctx) => {
     await ctx.reply('Приветсвую меня зовут @NewBotSim')
 })
-bot.command('all', async (ctx) => {
-    const keyboard = new Keyboard().text('Settings').text('About').text('Сыллка на группу автора')
+bot.command('main', async (ctx) => {
+    const mainkeyboard = new InlineKeyboard().webApp('App', link)
     await ctx.reply('Передаю вам свой пульт!', {
-        reply_markup: keyboard
+        reply_markup: mainkeyboard
     })
 })
+
 bot.hears([/блять/, /сука/, /ебан/, /гондон/, /хуй/, /пизда/], async (ctx) => {
     await bot.api.deleteMessage(ctx.chat.id, ctx.msgId)
     await ctx.reply('Не матерись!')
+})
+bot.hears('О нас', async (ctx) => {
+    await ctx.reply('Автор бота:@simfort')
+})
+bot.hears('Сыллка на группу автора', async (ctx) => {
+    await ctx.reply('https://t.me/getcode_front')
 })
 
 bot.start()
